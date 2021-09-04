c52d420cb948689a463c13e3664b5df841fc8e00
artevance <agustinus.michael29@gmail.com>
2021-09-04 11:08:52 +0700

    Make log controllers

---
+++ app/Http/Controllers/Ajax/Log/ActivityLogController.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace App\Http\Controllers\Ajax\Log;
+
+use Bsb\Foundation\Http\Controller;
+use App\Models\Log\ActivityLog;
+use Illuminate\Http\Request;
+use App\Http\Requests\Ajax\Log\ActivityLog\{
+    IndexRequest,
+    ShowRequest,
+    DestroyRequest,
+    RevealLatestRequest,
+    RevealTrafficRequest,
+};
+use App\Tasks\Log\ActivityLog\DestroyTask;
+use App\Widgets\Log\ActivityLog\{
+    LatestWidget,
+    TrafficWidget,
+};
+use App\Transformer\SingleCollections\Log\ActivityLogSingleCollection;
+use App\Transformer\PaginatedCollections\Log\ActivityLogPaginatedCollection;
+use App\Transformer\RelatedResources\Log\ActivityLogRelatedResource;
+
+class ActivityLogController extends Controller
+{
+    /**
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function manifest()
+    {
+        return response()->json(
+            new ActivityLogSingleCollection(
+                ActivityLog::all()
+            ),
+            200
+        );
+    }
+
+    /**
+     * @param  \App\Http\Requests\Ajax\Log\IndexRequest  $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function index(IndexRequest $request)
+    {
+        return response()->json(
+            new ActivityLogPaginatedCollection(
+                ActivityLog::where(
+                    $this->buildWhere($request)
+                        ->with($request)
+                        ->index('user_id')->mode('id')
+                        ->index('activity_log_type')->mode('strict_string')
+                        ->done()
+                )
+                    ->orderBy($request->input('sort'), $request->input('order'))
+                    ->paginate($request->input('count'))
+            ),
+            200
+        );
+    }
+
+    /**
+     * @param  \App\Http\Requests\Res\Log\ShowRequest  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function show(ShowRequest $request, $id)
+    {
+        return response()->json(
+            new ActivityLogRelatedResource(
+                ActivityLog::findOrFail($id)
+            ),
+            200
+        );
+    }
+
+    /**
+     * @param  \App\Http\Requests\Ajax\Log\DestroyRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(DestroyRequest $request)
+    {
+        $this->transmit(new DestroyTask, $request);
+        return response()->noContent();
+    }
+
+    /**
+     * @param  \App\Http\Requests\Ajax\Log\ActivityLog\RevealLatestRequest  $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function revealLatest(RevealLatestRequest $request)
+    {
+        return response()->json(
+            $this->reveal(new LatestWidget, $request)
+        );
+    }
+
+    /**
+     * @param  \App\Http\Requests\Ajax\Log\ActivityLog\RevealTrafficRequest  $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function revealTraffic(RevealTrafficRequest $request)
+    {
+        return response()->json(
+            $this->reveal(new TrafficWidget, $request)
+        );
+    }
+}
---
+++ app/Http/Controllers/Ajax/Log/AuthenticationLogController.php
@@ -0,0 +1,153 @@
+<?php
+
+namespace App\Http\Controllers\Ajax\Log;
+
+use Bsb\Foundation\Http\Controller;
+use App\Models\Log\AuthenticationLog;
+use Illuminate\Http\Request;
+use App\Http\Requests\Ajax\Log\AuthenticationLog\{
+    IndexRequest,
+    ShowRequest,
+    StoreLoginRequest,
+    DestroyRequest,
+    RevealLatestRequest,
+    RevealTrafficRequest,
+};
+use App\Tasks\Log\AuthenticationLog\{
+    StoreTask,
+    DestroyTask,
+};
+use App\Services\{
+    SanctumLoginService,
+    SanctumLogoutService,
+};
+use App\Widgets\Log\AuthenticationLog\{
+    LatestWidget,
+    TrafficWidget,
+};
+use App\Transformer\SingleCollections\Log\AuthenticationLogSingleCollection;
+use App\Transformer\PaginatedCollections\Log\AuthenticationLogPaginatedCollection;
+use App\Transformer\RelatedResources\Log\AuthenticationLogRelatedResource;
+
+class AuthenticationLogController extends Controller
+{
+    /**
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function manifest()
+    {
+        return response()->json(
+            new AuthenticationLogSingleCollection(
+                AuthenticationLog::all()
+            ),
+            200
+        );
+    }
+
+    /**
+     * @param  \App\Http\Requests\Ajax\Log\AuthenticationLog\IndexRequest  $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function index(IndexRequest $request)
+    {
+        return response()->json(
+            new AuthenticationLogPaginatedCollection(
+                AuthenticationLog::where(
+                    $this->buildWhere($request)
+                        ->with($request)
+                        ->index('user_id')->mode('id')
+                        ->index('state')->mode('boolean')
+                        ->done()
+                )
+                    ->orderBy($request->input('sort'), $request->input('order'))
+                    ->paginate($request->input('count'))
+            ),
+            200
+        );
+    }
+
+    /**
+     * @param  \App\Http\Requests\Res\Log\AuthenticationLog\ShowRequest  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function show(ShowRequest $request, $id)
+    {
+        return response()->json(
+            new AuthenticationLogRelatedResource(
+                AuthenticationLog::findOrFail($id)
+            ),
+            200
+        );
+    }
+
+    /**
+     * @param  \App\Http\Requests\Ajax\Log\AuthenticationLog\StoreLoginRequest  $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function storeLogin(StoreLoginRequest $request)
+    {
+        return response()->json(
+            new AuthenticationLogRelatedResource(
+                $this->transmit(
+                    new StoreTask,
+                    $request,
+                    $this->manage(new SanctumLoginService, $request),
+                    true,
+                )
+            ),
+            200
+        );
+    }
+
+    /**
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function storeLogout(Request $request)
+    {
+        return response()->json(
+            new AuthenticationLogRelatedResource(
+                $this->transmit(
+                    new StoreTask,
+                    $request,
+                    $this->manage(new SanctumLogoutService, $request),
+                    false,
+                )
+            ),
+            200
+        );
+    }
+
+    /**
+     * @param  \App\Http\Requests\Ajax\Log\DestroyRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(DestroyRequest $request)
+    {
+        $this->transmit(new DestroyTask, $request);
+        return response()->noContent();
+    }
+
+    /**
+     * @param  \App\Http\Requests\Ajax\Log\AuthenticationLog\RevealLatestRequest  $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function revealLatest(RevealLatestRequest $request)
+    {
+        return response()->json(
+            $this->reveal(new LatestWidget, $request)
+        );
+    }
+
+    /**
+     * @param  \App\Http\Requests\Ajax\Log\AuthenticationLog\RevealTrafficRequest  $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function revealTraffic(RevealTrafficRequest $request)
+    {
+        return response()->json(
+            $this->reveal(new TrafficWidget, $request)
+        );
+    }
+}
