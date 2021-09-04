3320efed9fbf1a3f8f6edb3c5ddf6f43102b3161
artevance <agustinus.michael29@gmail.com>
2021-09-04 12:50:27 +0700

    Make csrf cookie controller

--- routes/ajax/utils.php
+++ routes/ajax/utils.php
@@ -6,3 +6,4 @@ Route::get('authenticated', 'AuthenticatedController@index');
 Route::get('version_check', 'VersionCheckController@index');
 Route::get('language_resources', 'LanguageResourcesController@index');
 Route::get('authorization', 'AuthorizationController@index');
+Route::get('csrf_cookie', 'CsrfCookieController@show');
