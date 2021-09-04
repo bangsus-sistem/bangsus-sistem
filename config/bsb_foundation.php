8ffa40db65071e969f8446d5fff620b3ce4873f7
artevance <agustinus.michael29@gmail.com>
2021-09-04 09:32:00 +0700

    Make bsb foundation config file

---
+++ config/bsb_foundation.php
@@ -0,0 +1,35 @@
+<?php
+
+return [
+    'http' => [
+        'request' => [
+            'feature' => [
+                'model' => \App\Models\Auth\Feature::class,
+            ],
+            'widget' => [
+                'model' => \App\Models\Auth\Widget::class,
+            ],
+            'report' => [
+                'model' => \App\Models\Auth\Report::class,
+            ],
+        ],
+    ],
+    'macro' => [
+        'register' => [
+            'blueprints' => true,
+            'rules' => true,
+        ],
+        'kernel' => \App\Macro\Kernel::class,
+        'config' => [
+            'rule' => [
+                'prefix' => 'bsb',
+            ],
+        ],
+    ],
+    'observer' => [
+        'kernel' => [
+            \App\Observers\Kernel::class,
+            \App\Observers\Kernel::class
+        ],
+    ],
+];
