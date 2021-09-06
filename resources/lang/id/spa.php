729972246b681b743c00b98e3d513dd1905a2179
artevance <agustinus.michael29@gmail.com>
2021-09-06 11:41:45 +0700

    Make SPA lang file for id locale

---
+++ resources/lang/id/spa.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    'errors' => [
+        'default' => 'Terjadi kesalahan.',
+        'auth' => [
+            'authenticating' => 'Login gagal.',
+            'unauthenticated' => 'Sesi anda sudah habis.'
+        ],
+        'not_found' => 'Data tidak ditemukan.',
+        'form' => 'Cek kembali data anda.',
+    ],
+    'success' => [
+        'form' => 'Sukses.',
+    ],
+    
+    'dictionary' => [
+        'now' => 'sekarang',
+    ],
+
+];
