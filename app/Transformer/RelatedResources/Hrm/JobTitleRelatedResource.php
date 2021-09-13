281e6dfee1d0e55c7b543f676927ecb0e4990ca4
artevance <agustinus.michael29@gmail.com>
2021-09-13 16:57:10 +0700

    Make hrm related resources

---
+++ app/Transformer/RelatedResources/Hrm/JobTitleRelatedResource.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Transformer\RelatedResources\Hrm;
+
+use Bsb\Transformer\RelatedResource;
+use App\Transformer\SingleResources\Auth\UserSingleResource;
+
+class JobTitleRelatedResource extends RelatedResource
+{
+    /**
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'code' => $this->code,
+            'name' => $this->name,
+            'division' => new DivisionSingleResource($this->division),
+            'active' => (bool) $this->active,
+            'description' => $this->description,
+            'note' => $this->note,
+            'user_create' => new UserSingleResource($this->userCreate),
+            'user_update' => new UserSingleResource($this->userUpdate),
+            'user_delete' => new UserSingleResource($this->userDelete),
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+            'deleted_at' => $this->deleted_at,
+        ];
+    }
+}
