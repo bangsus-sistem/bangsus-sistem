93994cfaf10e5b5432549e131f144de3fe5f5ee9
artevance <agustinus.michael29@gmail.com>
2021-09-13 17:17:42 +0700

    Fix wrong class name

--- app/Transformer/PaginatedResources/Hrm/GenderPaginatedResource.php
+++ app/Transformer/PaginatedResources/Hrm/GenderPaginatedResource.php
@@ -4,7 +4,7 @@ namespace App\Transformer\PaginatedResources\Hrm;
 
 use Bsb\Foundation\Transformer\PaginatedResource;
 
-class GenderTypePaginatedResource extends PaginatedResource
+class GenderPaginatedResource extends PaginatedResource
 {
     /**
      * @param  \Illuminate\Http\Request  $request
