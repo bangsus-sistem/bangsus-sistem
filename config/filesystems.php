0ce15b0b5cac12188981672e81d6740880cc1a96
artevance <agustinus.michael29@gmail.com>
2021-09-14 17:05:01 +0700

    Add image and file driver

--- config/filesystems.php
+++ config/filesystems.php
@@ -42,6 +42,20 @@ return [
             'visibility' => 'public',
         ],
 
+        'image' => [
+            'driver' => 'local',
+            'root' => storage_path('app/public/image'),
+            'url' => env('APP_URL').'/storage',
+            'visibility' => 'public',
+        ],
+
+        'file' => [
+            'driver' => 'local',
+            'root' => storage_path('app/public/file'),
+            'url' => env('APP_URL').'/storage',
+            'visibility' => 'public',
+        ],
+
         's3' => [
             'driver' => 's3',
             'key' => env('AWS_ACCESS_KEY_ID'),
