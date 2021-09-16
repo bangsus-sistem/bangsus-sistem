7896e428ba6bdb4e575822f44821972225e71193
artevance <agustinus.michael29@gmail.com>
2021-09-16 09:35:52 +0700

    Add caster and attendance date attribute

--- app/Models/Hrm/Attendance.php
+++ app/Models/Hrm/Attendance.php
@@ -19,4 +19,27 @@ class Attendance extends Model
 {
     use SoftDeletes, HasEmployee, HasBranch, HasAttendanceType, HasImage,
         HasUserTimestamps, HasUserDelete;
+
+    /**
+     * @var array
+     */
+    protected $casts = [
+        'schedule_in_datetime' => 'datetime:Y-m-d H:i:s',
+        'schedule_out_datetime' => 'datetime:Y-m-d H:i:s',
+        'attendance_in_datetime' => 'datetime:Y-m-d H:i:s',
+        'attendance_out_datetime' => 'datetime:Y-m-d H:i:s',
+    ];
+
+    /**
+     * Make a flexible attendance date attribute based on either the schedule
+     * datetime or the atttendance datetime.
+     * 
+     * @return string
+     */
+    public function getAttendanceDateAttribute()
+    {
+        return $this->schedule_in_datetime != null
+            ?   $this->schedule_in_datetime->format('Y-m-d')
+            :   $this->attendance_in_datetime->format('Y-m-d');
+    }
 }
