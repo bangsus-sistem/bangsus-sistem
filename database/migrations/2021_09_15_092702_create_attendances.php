37e2b5e26a1ee6b704e4a3f1e7e1e0f12e10bad7
artevance <agustinus.michael29@gmail.com>
2021-09-20 13:45:04 +0700

    Adding some field to branch

--- database/migrations/2021_09_15_092702_create_attendances.php
+++ database/migrations/2021_09_15_092702_create_attendances.php
@@ -18,12 +18,14 @@ class CreateAttendances extends Migration
             $table->employee();
             $table->branch();
             $table->attendanceType();
-            $table->image();
+            $table->image('image_in_id');
+            $table->image('image_out_id');
             $table->timestamp('schedule_in_datetime')->nullable();
             $table->timestamp('schedule_out_datetime')->nullable();
             $table->timestamp('attendance_in_datetime')->nullable();
             $table->timestamp('attendance_out_datetime')->nullable();
-            $table->point('position')->nullable();
+            $table->point('position_in')->nullable();
+            $table->point('position_out')->nullable();
             $table->userTimestamps();
             $table->userDelete();
             $table->timestamps();
