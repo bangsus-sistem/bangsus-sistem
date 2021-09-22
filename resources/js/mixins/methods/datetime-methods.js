55f2b3b30008310974936b37ee0851e9ad5fb815
artevance <agustinus.michael29@gmail.com>
2021-09-22 16:06:01 +0700

    Add iso datetime method

--- resources/js/mixins/methods/datetime-methods.js
+++ resources/js/mixins/methods/datetime-methods.js
@@ -13,7 +13,7 @@ const datetimeMethods = {
      * 
      * @deprecated
      * @param  {String}  datetime
-     * @return {Number}
+     * @return {String}
      */
     standardDatetime(datetime = null) {
         moment.locale('id')
@@ -25,12 +25,23 @@ const datetimeMethods = {
      * 
      * @deprecated
      * @param  {String}  datetime
-     * @return {Number}
+     * @return {String}
      */
     standardDate(datetime = null) {
         moment.locale('id')
         return datetime == null ? '-' : moment(datetime).format('dddd, Do MMMM YYYY')
     },
+
+    /**
+     * Parse the timestamp to ISO standard date for HTML component.
+     * 
+     * @param  {String}  datetime
+     * @return {String}
+     */
+    isoDatetime(datetime = null) {
+        moment.locale('id')
+        return datetime == null ? null : moment(datetime).format('YYYY-MM-DD[T]HH:mm:ss')
+    }
 
     // --------------------------------------------------------------
 
