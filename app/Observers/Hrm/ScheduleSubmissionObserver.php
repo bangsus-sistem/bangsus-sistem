1df2045eb56a04eae82a894577dd14c0e978d752
artevance <agustinus.michael29@gmail.com>
2021-09-23 16:07:05 +0700

    Fix admitting algorithm

--- app/Observers/Hrm/ScheduleSubmissionObserver.php
+++ app/Observers/Hrm/ScheduleSubmissionObserver.php
@@ -18,23 +18,25 @@ class ScheduleSubmissionObserver extends Observer
                 return $query->whereDate('schedule_in_datetime', $scheduleSubmission->schedule_in_datetime->format('Y-m-d'))
                     ->orWhere(function ($query) use ($scheduleSubmission) {
                         return $query->whereNull('schedule_in_datetime')
-                            ->whereDate('attendance_in_datetime', $scheduleSubmission->schedule_in_datetime)->format('Y-m-d');
+                            ->whereDate('attendance_in_datetime', $scheduleSubmission->schedule_in_datetime->format('Y-m-d'));
                     });
             }
         )
             ->where('employee_id', $scheduleSubmission->employee_id)
             ->where('branch_id', $scheduleSubmission->branch_id)
+            ->where('attendance_type_id', $scheduleSubmission->attendance_type_id)
             ->first();
 
         if (is_null($attendance)) {
             $attendance = new Attendance;
             $attendance->employee_id = $scheduleSubmission->employee_id;
-            $attendace->branch_id = $scheduleSubmission->branch_id;
+            $attendance->branch_id = $scheduleSubmission->branch_id;
+            $attendance->attendance_type_id = $scheduleSubmission->attendance_type_id;
         }
 
         $attendance->schedule_in_datetime = $scheduleSubmission->schedule_in_datetime;
         $attendance->schedule_out_datetime = $scheduleSubmission->schedule_out_datetime;
 
-        return true;
+        $attendance->save();
     }
 }
