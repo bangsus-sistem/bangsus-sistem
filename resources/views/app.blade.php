749f8943366c71596955ce1d8fd15e7517f9b73c
artevance <agustinus.michael29@gmail.com>
2021-09-27 15:04:53 +0700

    Prevent auto-translating

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,9 +1,10 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
+<head translate="no">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="google" content="notranslate">
     <title>Bangsus Sistem</title>
     <link rel="stylesheet" href="/css/app.css?version={{ $version['app'] }}">
 </head>
