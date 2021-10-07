c544fb6cf0ce199862d9904cdbc3ac648adeba60
artevance <agustinus.michael29@gmail.com>
2021-10-07 17:08:15 +0700

    Add detail validation exception handler

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,6 +3,8 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Validation\ValidationException;
+use Illuminate\Support\Arr;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -37,5 +39,25 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+    
+    /**
+     * Convert a validation exception into a JSON response.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Validation\ValidationException  $exception
+     * @return \Illuminate\Http\JsonResponse
+     */
+    protected function invalidJson($request, ValidationException $exception)
+    {
+        $errors = [];
+
+        foreach ($exception->errors() as $key => $value)
+            Arr::set($errors, $key, $value);
+
+        return response()->json([
+            'errors' => $errors,
+            'message' => 'The given data was invalid.',
+        ], $exception->status);
     }
 }
