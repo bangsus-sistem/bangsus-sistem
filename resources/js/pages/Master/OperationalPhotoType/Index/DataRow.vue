f80a3ef8631c391b9be895c4b59fb1d283c22bbc
agusmikevar <90199402+agusmikevar@users.noreply.github.com>
2021-10-24 13:25:13 +0700

    Add button to the Operational Photo Type children pages

--- resources/js/pages/Master/OperationalPhotoType/Index/DataRow.vue
+++ resources/js/pages/Master/OperationalPhotoType/Index/DataRow.vue
@@ -25,6 +25,11 @@
                             Denda
                         </bsb-button-router-link>
                     </bsb-access-wrapper>
+                    <bsb-access-wrapper module-ref="minimum_operational_photo" action-ref="index">
+                        <bsb-button-router-link :to="{ name: 'master.minimumOperationalPhoto', params: { operationalPhotoTypeId: item['id'] } }" color="secondary" size="sm">
+                            Minimum
+                        </bsb-button-router-link>
+                    </bsb-access-wrapper>
                 </bsb-td>
             </tr>
         </template>
@@ -50,6 +55,11 @@
                         <bsb-access-wrapper module-ref="operational_photo_penalty_type" action-ref="index">
                             <bsb-button-router-link :to="{ name: 'master.operationalPhotoPenaltyType', params: { operationalPhotoTypeId: item['id'] } }" color="secondary" size="sm">
                                 Denda
+                            </bsb-button-router-link>
+                        </bsb-access-wrapper>
+                        <bsb-access-wrapper module-ref="minimum_operational_photo" action-ref="index">
+                            <bsb-button-router-link :to="{ name: 'master.minimumOperationalPhoto', params: { operationalPhotoTypeId: item['id'] } }" color="secondary" size="sm">
+                                Minimum
                             </bsb-button-router-link>
                         </bsb-access-wrapper>
                     </template>
